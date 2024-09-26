450f46010b13a8a3af13e51871c3e353c795facd
Alle Santana <106606764+AliceRSant@users.noreply.github.com>
2024-09-26 19:56:34 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -16,3 +16,4 @@ async function vizualizarInformacoesGlobais() {
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
+vizualizarInformacoesGlobais()
